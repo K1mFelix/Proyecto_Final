70be32430ad8ecc4c8d67bc7eef27e1728b641c5
KimFelix <felisgabrielrios15@gmail.com>
2026-01-14 18:42:26 -0300

    Elimina el componente de login y actualiza el componente de home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,13 +2,6 @@ import React from 'react'
 
 const home = () => {
   return (
-<<<<<<< HEAD
-    <div>home</div>
-  )
-}
-
-export default home
-=======
     <div>
       
     </div>
@@ -16,4 +9,3 @@ export default home
 }
 
 export default home
->>>>>>> 586c0e5a89384ba0c1d026df32165853866daa21
